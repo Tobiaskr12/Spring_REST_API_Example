c6d12aa0b820dcf7e0b4c6813d9ad124a3777691
Tobias Kristensen <tobiaskristensen12@gmail.com>
2022-11-30 18:35:04 +0100

    Initial commit

---
+++ src/main/java/sdu/sem3/contextaware/HTTP_API/HttpApiApplication.java
@@ -0,0 +1,13 @@
+package sdu.sem3.contextaware.HTTP_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sdu/sem3/contextaware/HTTP_API/HttpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sdu.sem3.contextaware.HTTP_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
